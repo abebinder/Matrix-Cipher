af039ed81ec3ba2a6a31fef353ded54bbad073fc
Robert Malone <robert.malone@ColoradoCollege.edu>
2017-09-11 14:39:14 -0600

    Everything workds

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) 
-	{d
+	{
 		MatrixMultiplications calculator = new MatrixMultiplications();
 		Inverse inverseCalc = new Inverse();
 		double[][] A = {{1,2,3},{4,5,6},{7,8,9}};
