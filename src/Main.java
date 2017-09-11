965909acb9232d34278d7e5a317afbeeecfc5755
Robert Malone <robert.malone@ColoradoCollege.edu>
2017-09-11 13:50:02 -0600

    New Branch Test

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		MatrixOperations calculator = new MatrixOperations();
-		Double[][] A = {{1,2,3},{4.0,5,6},{7,8,9}};
-		
+		Double[][] A = {{1,2,3},{4,5,6},{7,8,9}};
 		
 	}
 
